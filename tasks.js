d031e02a6cf91d239e04f63d9d7198f1bb3fb791
HakanK003 <hkaratashk3@gmail.com>
2022-10-12 22:58:46 -0400

    Task 1 and Task 2

--- tasks.js
+++ tasks.js
@@ -1,1 +1,52 @@
-//Jest JavaScript
+//JavaScript Tasks - Meta JS Course
+
+
+//Task 1 - Declaring variables
+var petDog = 'Rex';
+var petCat = 'Pepper';
+
+var petDog = 'Rex'; 
+console.log(petDog);
+
+console.log("My pet cat's name is: " + petCat);
+
+var catSound = "purr";
+var dogSound = "woof";
+
+console.log(petDog, "says", dogSound);
+console.log(petCat, "says", catSound);
+
+catSound = "meow";
+
+console.log(petCat, "now says", catSound);
+
+
+//Task 2 - Advanced use of operators
+
+var score = 8;
+console.log("Mid-level skills:", score > 0 && score < 10)
+
+var timeRemaining = 0;
+var energy = 10;
+console.log("Game over:", timeRemaining == 0 || energy == 0);
+
+var num1 = 2;
+var num2 = 5;
+var test1 = num1 % 2;
+var test2 = num2 % 2;
+var result1 = test1 == 0;
+var result2 = test2 == 0;
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2);
+
+console.log(5 + 10); // 15
+
+var now = "Now in ";
+var three = 3;
+var d = "D!"
+console.log(now + three + d); // "Now in 3D!"
+
+var counter = 0;
+counter += 5;
+counter += 3;
+console.log(counter); // 8
