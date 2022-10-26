64346b9e3b55c319e2d9d4ed50b8067face0cc3a
HakanK003 <hkaratashk3@gmail.com>
2022-10-25 23:24:17 -0400

    moved

--- jestUnitTests/jestTests.test.js
+++ jestUnitTests/jestTests.test.js
@@ -1,6 +1,6 @@
 const { exportAllDeclaration } = require("@babel/types");
 const { default: TestRunner } = require("jest-runner");
-const addFive = require(`./addFive`);
+const addFive = require(`../javaScriptPractice/addFive`);
 
 test('returns the number plus 5', () => {
     expect(addFive(1)).toBe(6);
